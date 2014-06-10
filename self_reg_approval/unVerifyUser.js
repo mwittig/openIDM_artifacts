0799e1da80425eaa9560a4ea172d36f714de8d93
smof <simon_moffatt@warpmail.net>
2014-06-10 11:53:08 +0100

    Added the unverify response on change

--- self_reg_approval/unVerifyUser.js
+++ self_reg_approval/unVerifyUser.js
@@ -1,2 +1,10 @@
-//patch the source object to replace verified to false
-openidm.patch("managed/user/" + source._id, null, [{"replace": "/verified", "value": false}]);
+//Only do this stuff if user was previously verified; otherwise do nada
+if (source.verified == 'true') {
+	
+	//patch the source object to replace verified to false
+	openidm.patch("managed/user/" + source._id, null, [{"replace": "/verified", "value": "false"}]);
+	
+}
+
+//required by OpenIDM
+"ASYNC";
