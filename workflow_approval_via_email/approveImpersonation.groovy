00ddb6106cb40686cd3568cb35982c7c57833e68
smof <simon_moffatt@warpmail.net>
2016-05-31 16:48:15 +0100

    Added basic exception handling to workflow approval endpoints

--- workflow_approval_via_email/approveImpersonation.groovy
+++ workflow_approval_via_email/approveImpersonation.groovy
@@ -9,57 +9,77 @@
 
 //Pull in args given via URL - now this creates an issue as the original Groovy generated object [] was serialised to String for transport
 //Response object
-def response = [:]
-
-def submittedPayload = request.additionalParameters.payload
+def res = [:]
 
 //Check that args via URL are actually present
-if (submittedPayload == null) {
-	
-	throw "Missing Arg : payload";
-	
-} 
-//Rehydrate the crypto object from the submitted string
-def reconstructedCryptoObject = (new groovy.json.JsonSlurper()).parseText(submittedPayload)
+if (request.additionalParameters.payload == null) {
 
-//Decrypt
-def decryptedObject = openidm.decrypt(reconstructedCryptoObject)
-def submittedUser = decryptedObject.user
-def submittedApprovalCode = decryptedObject.code
+	res['message'] = "Missing Arg : payload"
 
-//Find the necessary workflow task that this request is going complete
-//Find tasks with assignee of the end user
-def queryParameters = [_queryId:'filtered-query', assignee:submittedUser]
-def queryResult = openidm.query("workflow/taskinstance/", queryParameters)
-
-//Pull out the specific task id
-def taskId = queryResult.result[0]._id //Note this assumes only 1 open task is found, needs a loop if demoing and have lots of open tasks
-//Read that specific task
-def task = openidm.read("workflow/taskinstance/" + taskId)
-//Find the approval verification code embedded within this task
-def taskApprovalCode = task.variables.approvalCode
-	
-//Check if the code in the payload matches the code in the task instance
-if (taskApprovalCode == submittedApprovalCode) {
-		
-	//Complete the workflow - using external REST as openid.action against workflow/taskinstane doesn't support _action=complete 
-	completeWorkflowResponse = openidm.action("external/rest", "call", [
-		"url": "http://openidm.example.com:8081/openidm/workflow/taskinstance/"+taskId+"?_action=complete",
-		"method": "POST",
-		"headers": [
-			"Content-type": "application/json",
-			"X-OpenIDM-Username": "openidm-admin",
-			"X-OpenIDM-Password": "Passw0rd",
-			"Accept-Language": "en-US,en"
-		],
-		"body": '{"userDecision": "accept"}'
-	]);
-	response['message'] = completeWorkflowResponse
-	
 } else {
 
-	response['message'] = "Task not completed"
+	def submittedPayload = request.additionalParameters.payload
+
+	//Rehydrate the crypto object from the submitted string
+	def reconstructedCryptoObject = (new groovy.json.JsonSlurper()).parseText(submittedPayload)
+
+	//Check if object is decryptable
+	if (openidm.isEncrypted(reconstructedCryptoObject) == true) {
+
+		//Decrypt
+		def decryptedObject = openidm.decrypt(reconstructedCryptoObject)
+		def submittedUser = decryptedObject.user
+		def submittedApprovalCode = decryptedObject.code
+
+		//Find the necessary workflow task that this request is going complete
+		//Find tasks with assignee of the end user
+		def queryParameters = [_queryId:'filtered-query', assignee:submittedUser]
+		def queryResult = openidm.query("workflow/taskinstance/", queryParameters)
+
+		if (queryResult.result.size != 0) {
+
+			//Pull out the specific task id
+			def taskId = queryResult.result[0]._id //Note this assumes only 1 open task is found, needs a loop if demoing and have lots of open tasks
+			//Read that specific task
+			def task = openidm.read("workflow/taskinstance/" + taskId)
+			//Find the approval verification code embedded within this task
+			def taskApprovalCode = task.variables.approvalCode
+
+			//Check if the code in the payload matches the code in the task instance
+			if (taskApprovalCode == submittedApprovalCode) {
+
+				//Complete the workflow - using external REST as openid.action against workflow/taskinstane doesn't support _action=complete
+				completeWorkflowResponse = openidm.action("external/rest", "call", [
+					"url": "http://openidm.example.com:8081/openidm/workflow/taskinstance/"+taskId+"?_action=complete",
+					"method": "POST",
+					"headers": [
+						"Content-type": "application/json",
+						"X-OpenIDM-Username": "openidm-admin",
+						"X-OpenIDM-Password": "Passw0rd",
+						"Accept-Language": "en-US,en"
+					],
+					"body": '{"userDecision": "accept"}'
+				]);
+				res['message'] = completeWorkflowResponse
+
+			} else {
+
+				res['message'] = "Task not completed"
+			}
+
+
+
+		} else {
+
+			res['message'] = "Task not found"
+
+		}
+
+
+	}
+
+
 }
-	
-	
+return res
+
 
--- workflow_approval_via_email/rejectImpersonation.groovy
+++ workflow_approval_via_email/rejectImpersonation.groovy
@@ -1,5 +1,5 @@
 //simon.moffatt@forgerock.com 20/05/16
-//Manages the workflow task approval process
+//Manages the workflow task rejection process
 //
 //Verb: GET
 //Args: payload - encrypted payload
@@ -9,57 +9,76 @@
 
 //Pull in args given via URL - now this creates an issue as the original Groovy generated object [] was serialised to String for transport
 //Response object
-def response = [:]
-
-def submittedPayload = request.additionalParameters.payload
+def res = [:]
 
 //Check that args via URL are actually present
-if (submittedPayload == null) {
-	
-	throw "Missing Arg : payload";
-	
-} 
-//Rehydrate the crypto object from the submitted string
-def reconstructedCryptoObject = (new groovy.json.JsonSlurper()).parseText(submittedPayload)
+if (request.additionalParameters.payload == null) {
 
-//Decrypt
-def decryptedObject = openidm.decrypt(reconstructedCryptoObject)
-def submittedUser = decryptedObject.user
-def submittedRejectCode = decryptedObject.code
+	res['message'] = "Missing Arg : payload"
 
-//Find the necessary workflow task that this request is going complete
-//Find tasks with assignee of the end user
-def queryParameters = [_queryId:'filtered-query', assignee:submittedUser]
-def queryResult = openidm.query("workflow/taskinstance/", queryParameters)
-
-//Pull out the specific task id
-def taskId = queryResult.result[0]._id //Note this assumes only 1 open task is found, needs a loop if demoing and have lots of open tasks
-//Read that specific task
-def task = openidm.read("workflow/taskinstance/" + taskId)
-//Find the approval verification code embedded within this task
-def taskApprovalCode = task.variables.rejectCode
-	
-//Check if the code in the payload matches the code in the task instance
-if (taskApprovalCode == submittedRejectCode) {
-		
-	//Complete the workflow - using external REST as openid.action against workflow/taskinstane doesn't support _action=complete 
-	completeWorkflowResponse = openidm.action("external/rest", "call", [
-		"url": "http://openidm.example.com:8081/openidm/workflow/taskinstance/"+taskId+"?_action=complete",
-		"method": "POST",
-		"headers": [
-			"Content-type": "application/json",
-			"X-OpenIDM-Username": "openidm-admin",
-			"X-OpenIDM-Password": "Passw0rd",
-			"Accept-Language": "en-US,en"
-		],
-		"body": '{"userDecision": "reject"}'
-	]);
-	response['message'] = completeWorkflowResponse
-	
 } else {
 
-	response['message'] = "Task not completed"
+	def submittedPayload = request.additionalParameters.payload
+
+	//Rehydrate the crypto object from the submitted string
+	def reconstructedCryptoObject = (new groovy.json.JsonSlurper()).parseText(submittedPayload)
+
+	//Check if object is decryptable
+	if (openidm.isEncrypted(reconstructedCryptoObject) == true) {
+
+		//Decrypt
+		def decryptedObject = openidm.decrypt(reconstructedCryptoObject)
+		def submittedUser = decryptedObject.user
+		def submittedRejectCode = decryptedObject.code
+
+		//Find the necessary workflow task that this request is going complete
+		//Find tasks with assignee of the end user
+		def queryParameters = [_queryId:'filtered-query', assignee:submittedUser]
+		def queryResult = openidm.query("workflow/taskinstance/", queryParameters)
+
+		if (queryResult.result.size != 0) {
+
+			//Pull out the specific task id
+			def taskId = queryResult.result[0]._id //Note this assumes only 1 open task is found, needs a loop if demoing and have lots of open tasks
+			//Read that specific task
+			def task = openidm.read("workflow/taskinstance/" + taskId)
+			//Find the reject verification code embedded within this task
+			def taskRejectCode = task.variables.rejectCode
+
+			//Check if the code in the payload matches the code in the task instance
+			if (taskRejectCode == submittedRejectCode) {
+
+				//Complete the workflow - using external REST as openid.action against workflow/taskinstane doesn't support _action=complete
+				completeWorkflowResponse = openidm.action("external/rest", "call", [
+					"url": "http://openidm.example.com:8081/openidm/workflow/taskinstance/"+taskId+"?_action=complete",
+					"method": "POST",
+					"headers": [
+						"Content-type": "application/json",
+						"X-OpenIDM-Username": "openidm-admin",
+						"X-OpenIDM-Password": "Passw0rd",
+						"Accept-Language": "en-US,en"
+					],
+					"body": '{"userDecision": "reject"}'
+				]);
+				res['message'] = completeWorkflowResponse
+
+			} else {
+
+				res['message'] = "Task not completed"
+			}
+
+
+
+		} else {
+
+			res['message'] = "Task not found"
+
+		}
+
+
+	}
+
+
 }
-	
-	
+return res
 
